8b80cce7c42dceb4c752952a48c6f724e45a48ec
Serdar Olmez <serdarolmez0@gmail.com>
2023-03-25 14:56:48 +0100

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+mongoose.connect('mongodb+srv://serdarolmez0:29031992Se@cluster0.vt9htkb.mongodb.net/piquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  
+
+module.exports = app;
